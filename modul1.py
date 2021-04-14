4de5d9ca39a6640ceb14b4e5058de83be4f5626e
OvidiuM91 <ovidiu_monea@outlook.com>
2021-04-14 21:27:18 +0300

    Add modul1

---
+++ modul1.py
@@ -0,0 +1,36 @@
+print('Hello World')
+print('print\u200b("")')
+# print​("")
+HELLO_WORLD = 'Hello World'
+print(HELLO_WORLD)
+
+# variabilele se scriu cu litere mari sau litere mici si cu spatiu: _ (este recomandat in Python)
+
+name = 'Ovidiu\n'
+# name='Ovidiu '
+# print(3*name)
+# print(3 * (name+ ' '))
+# print(name, name, name)
+# print(name,name,name, sep='#', end='')
+
+# print(100*name)
+
+print(80 * '#')
+print(3 * name)
+print(name * 3)
+print(print)
+
+# print int
+print(3)
+print('3')
+
+print('Triunghi: ')
+print("   / \ ")
+print("  /   \ ")
+print(' /     \ ')
+print("/_______\ ")
+print('Patrat: ')
+print(" _____")
+print("|     |")
+print("|_____|")
+
